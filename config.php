7a68f2b778dc4388abc7e6936b434d4c2158b7e6
mochsn01 <ochsner@gmail.com>
2012-01-03 12:27:34 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
-$app_id = "133225423421115";
+$app_id = "184724761626737";
 
-$app_secret = "f6d39ea886f0e45bc65e276e38216efa";
+$app_secret = "6124d4b8e167c8cd84fcdc42759c60cc";
 
 
 $cburl = 'http://befacebookking.com/fbstuff/charitry/';
 
-$canvas = 'https://apps.facebook.com/charitryapp/';
+$canvas = 'https://apps.facebook.com/magiccharityapp/';
 
 $fanpageLink = 'http://www.facebook.com/pages/ChariTry/206764136040354';
 
